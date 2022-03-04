0d33e584dd4303977dd31e4098f50fe5f62bef61
tristanlatr <tristanlatr@users.noreply.github.com>
2022-03-04 06:01:14 +0000

    Update nltk API documentations 83c99ee1012a06d7b42e3fedb31545291d161ea9

--- nltk/search-worker.js
+++ nltk/search-worker.js
@@ -1,19 +1,35 @@
-'use strict';
+// Try very hard to import these scripts (5 times). This avoid random errors: 
+// Uncaught NetworkError: Failed to execute 'importScripts' on 'WorkerGlobalScope': 
+// The script at 'https://pydoctor--500.org.readthedocs.build/en/500/api/lunr.js' failed to load.
 
-importScripts('lunr.js', 'ajax.js'); 
+for (let i = 0; i < 5; i++) {
+    try{
+        importScripts('lunr.js', 'ajax.js'); 
+        break;
+    } catch (ex){
+        if (i<5) {continue;}
+        else {throw ex;}
+    }
+}
 
 onmessage = function (message) { // -> {'results': [lunr results]}
     console.log("Message received from main script: ");
-    console.dir(message.data)
+    console.dir(message.data);
     
     if (!message.data.query) {
         throw ('No search query provided.');
     }
+    if (!message.data.indextype) {
+        throw ('No index type provided.');
+    }
+
+    // fullsearchindex.json includes field 'docstring', whereas searchindex.json.
+    indexurl = message.data.indextype === 'full' ?  "fullsearchindex.json" : "searchindex.json";
 
     // Launch the search
 
     // Build lunr index from serialized JSON constructed while generating the documentation. 
-    httpGet("searchindex.json", function(response) {
+    httpGet(indexurl, function(response) {
 
         // Parse JSON string into object
         let data = JSON.parse(response);
@@ -21,7 +37,28 @@ onmessage = function (message) { // -> {'results': [lunr results]}
         // Call lunr.Index.search
         // https://lunrjs.com/docs/lunr.Index.html
         let lunr_index = lunr.Index.load(data);
-        let search_results = lunr_index.search(message.data.query);
+
+        // Edit the parsed query clauses that are applicable for all fields (default) in order
+        // to remove the field 'kind' from the clause since this is only useful when specifically requested.
+        let query_fn = function (query) {
+            var parser = new lunr.QueryParser(message.data.query, query)
+            parser.parse()
+            query.clauses.forEach(clause => {
+                if (clause.fields == query.allFields){
+                    if (message.data.indextype === 'full'){
+                        // By default, for full index searches, only search on those 3 fields.
+                        clause.fields = ["name", "names", "qname", "docstring"];
+                    }
+                    else{
+                        // By default, for small index searches, only search on those 2 fields.
+                        clause.fields = ["name", "names", "qname"];
+                    }
+                }
+            });
+        }
+
+        let search_results = lunr_index.query(query_fn);
+        
         postMessage({'results':search_results});
 
         }, function(error){
